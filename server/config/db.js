7e67998b0259741bede9cb3c058a6b56b46570ce
keshavbhalla2006 <156053556+keshavbhalla2006@users.noreply.github.com>
2026-05-21 21:55:16 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,25 +1,24 @@
-const { Sequelize } = require('sequelize'); //Sequelize → Main class used to connect and interact with the database
-require('dotenv').config(); //dotenv → Loads environment variables from a .env file into process.env.
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host:    process.env.DB_HOST,
-    port:    process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
+
     dialectOptions: {
-      ssl: process.env.NODE_ENV === 'production' ? {
-        require: true,
-        rejectUnauthorized: false,
-      } : false,
+      ssl: {
+        ca: fs.readFileSync('./certs/ca.pem'),
+        rejectUnauthorized: true,
+      },
     },
   }
 );
 
-module.exports = sequelize; //Makes this database connection available in other files
-
-
-//e create one Sequelize instance — a single connection to the database. We export it so every model file can import it and use the same connection. logging: false suppresses the SQL query output in your terminal
+module.exports = sequelize;
